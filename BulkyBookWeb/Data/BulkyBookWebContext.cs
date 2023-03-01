aac8032872c25fd83b1265ca82687bf7d4b17028
yassinjm_ <yassinmoussamih7@gmail.com>
2023-03-01 08:26:44 +0100

    edit notification

---
+++ BulkyBookWeb/Data/BulkyBookWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Data
+{
+    public class BulkyBookWebContext : DbContext
+    {
+        public BulkyBookWebContext (DbContextOptions<BulkyBookWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BulkyBookWeb.Models.Category> Category { get; set; } = default!;
+    }
+}
